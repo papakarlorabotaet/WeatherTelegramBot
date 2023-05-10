7884de4cca5bede11c7fb64a24d198c148a29461
buttonwithmoustache <greenman-for-me@ya.ru>
2023-05-10 20:59:43 +0500

    first commit properties

--- src/main/java/org/fishve/vasyaev/WeatherBotApplication.java
+++ src/main/java/org/fishve/vasyaev/WeatherBotApplication.java
@@ -1,6 +1,6 @@
-package org.example;
+package org.fishve.vasyaev;
 
-public class Main {
+public class WeatherBotApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
